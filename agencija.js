bcf8a66cfc6d1b278d6df0325edfda4694e05457
NikolinaDolenec <nikolinadolenec12@gmail.com>
2026-09-11 17:15:41 +0200

    Add files via upload

---
+++ agencija.js
@@ -0,0 +1,12 @@
+function pozdrav(){
+    var ime=prompt("Upišite ime i prezime");
+    alert("Bok," + ime + "Dobrodošli u našu agenciju.");
+}
+
+function pokaziPoruku(){
+    alert("Dobro si kliknuo!");
+}
+
+function zamjeniBoju(){
+     document.body.bgColor="#d8f7ed";
+}
